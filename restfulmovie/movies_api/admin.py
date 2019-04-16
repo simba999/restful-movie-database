39dbd2974cd0845f2a4455b7e5bcff99520b74b3
simba999 <simba1512@yahoo.com>
2019-04-16 09:33:33 +0200

    Restructure files (one level down for heroku compliency)

--- restfulmovie/movies_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Movie, Rating
-
-admin.site.register(Movie)
-admin.site.register(Rating)
--- restfulmovie/movies_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoviesApiConfig(AppConfig):
-    name = 'movies_api'
